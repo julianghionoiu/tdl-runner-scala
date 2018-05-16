9df52970f35245cb6a7bf80b0ebb9850a2339075
neomatrix369 <sadhak001@gmail.com>
2018-05-17 00:09:56 +0100

    Workaround fix: a proper fix to the previous workaround, extract the coverage info from the xml file, save it in target/ and display onto the console

--- get_coverage_for_challenge.sh
+++ get_coverage_for_challenge.sh
@@ -7,27 +7,20 @@ set -o pipefail
 SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 CHALLENGE_ID=$1
-JACOCO_TEST_REPORT_XML_FILE="${SCRIPT_CURRENT_DIR}/target/scala-2.12/jacoco/report/jacoco.xml"
-SCALA_COVERAGE_INFO="${SCRIPT_CURRENT_DIR}/target/scala-code-coverage.txt"
+SCOVERAGE_REPORT_XML_FILE="${SCRIPT_CURRENT_DIR}/target/scala-2.12/scoverage-report/scoverage.xml"
+SCALA_CODE_COVERAGE_INFO="${SCRIPT_CURRENT_DIR}/target/scala-code-coverage.txt"
 
-sbt clean jacoco || true 1>&2
+sbt clean coverage test || true 1>&2
+sbt coverageReport || true 1>&2
 
-[ -e ${SCALA_COVERAGE_INFO} ] && rm ${SCALA_COVERAGE_INFO}
+[ -e ${SCALA_CODE_COVERAGE_INFO} ] && rm ${SCALA_CODE_COVERAGE_INFO}
 
-#
-# TODO: when one or more tests fails the jacoco coverage xml isn't created so we return nothing and exit with error code of -1.
-#       This is a work-around in case of missing coverage xml file.
-#       Proper solution would be to capture the results of the failing task and do nothing and proceed with execution
-#       Something to pick up at a later stage.
-#
-if [ -f "${JACOCO_TEST_REPORT_XML_FILE}" ]; then
-    COVERAGE_OUTPUT=$(xmllint --xpath '//package[@name="befaster/solutions/'${CHALLENGE_ID}'"]/counter[@type="INSTRUCTION"]' ${JACOCO_TEST_REPORT_XML_FILE})
-    MISSED=$(echo $COVERAGE_OUTPUT | awk '{print missed, $3}' | tr '="' ' ' | awk '{print $2}')
-    COVERED=$(echo $COVERAGE_OUTPUT | awk '{print missed, $4}' | tr '="' ' '| awk '{print $2}')
-
-    TOTAL_LINES=$((MISSED + $COVERED))
-    echo $(($COVERED * 100 / $TOTAL_LINES)) > ${SCALA_COVERAGE_INFO}
-    cat ${SCALA_COVERAGE_INFO}
+if [ -f "${SCOVERAGE_REPORT_XML_FILE}" ]; then
+    COVERAGE_OUTPUT=$(xmllint --xpath '//package[@name="befaster.solutions.'${CHALLENGE_ID}'"]/@statement-rate' ${SCOVERAGE_REPORT_XML_FILE})
+    COVERAGE_PERCENT=$(echo ${COVERAGE_OUTPUT} | tr '".' ' ' | awk '{print $2}')
+    COVERAGE_PERCENT=$(expr ${COVERAGE_PERCENT})
+    echo ${COVERAGE_PERCENT} > ${SCALA_CODE_COVERAGE_INFO}
+    cat ${SCALA_CODE_COVERAGE_INFO}
     exit 0
 else
     exit -1
