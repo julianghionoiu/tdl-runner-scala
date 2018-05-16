f07dbf55d39a879b6502e7ae2ed9e3025a979f48
neomatrix369 <sadhak001@gmail.com>
2018-05-16 23:05:16 +0100

    Code review comments: all issues raised have been raise, except the TODO: when one or more tests fails...

--- get_coverage_for_challenge.sh
+++ get_coverage_for_challenge.sh
@@ -8,11 +8,14 @@ SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 CHALLENGE_ID=$1
 JACOCO_TEST_REPORT_XML_FILE="${SCRIPT_CURRENT_DIR}/target/scala-2.12/jacoco/report/jacoco.xml"
+SCALA_COVERAGE_INFO="${SCRIPT_CURRENT_DIR}/target/scala-code-coverage.txt"
 
-sbt --error clean jacoco || true 1>&2
+sbt clean jacoco || true 1>&2
+
+[ -e ${SCALA_COVERAGE_INFO} ] && rm ${SCALA_COVERAGE_INFO}
 
 #
-# TODO: when one or more tests fails the jacoco coverage xml isn't created so we return 0.
+# TODO: when one or more tests fails the jacoco coverage xml isn't created so we return nothing and exit with error code of -1.
 #       This is a work-around in case of missing coverage xml file.
 #       Proper solution would be to capture the results of the failing task and do nothing and proceed with execution
 #       Something to pick up at a later stage.
@@ -23,7 +26,9 @@ if [ -f "${JACOCO_TEST_REPORT_XML_FILE}" ]; then
     COVERED=$(echo $COVERAGE_OUTPUT | awk '{print missed, $4}' | tr '="' ' '| awk '{print $2}')
 
     TOTAL_LINES=$((MISSED + $COVERED))
-    echo $(($COVERED * 100 / $TOTAL_LINES))
+    echo $(($COVERED * 100 / $TOTAL_LINES)) > ${SCALA_COVERAGE_INFO}
+    cat ${SCALA_COVERAGE_INFO}
+    exit 0
 else
-    echo 0
+    exit -1
 fi
