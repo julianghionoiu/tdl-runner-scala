ef98f10b51d7faab9fc8cc89aabe2b64ef77b942
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-06-23 15:26:07 +0100

    Update client to use List instead of Array for parameters

--- src/main/scala/befaster/EntryPointMapping.scala
+++ src/main/scala/befaster/EntryPointMapping.scala
@@ -1,5 +1,7 @@
 package befaster
 
+import java.util
+
 import befaster.solutions.ARRS.ArraySum
 import befaster.solutions.CHK.Checkout
 import befaster.solutions.FIZ.FizzBuzz
@@ -9,23 +11,22 @@ import befaster.solutions.SUM.Sum
 import com.google.gson.JsonElement
 
 import scala.collection.JavaConverters._
-
-
 import scala.collection.mutable.ListBuffer
 
+//noinspection ZeroIndexToHead
 object EntryPointMapping {
 
-  def sum(p: Array[JsonElement]): AnyRef = Sum.sum(p(0).getAsInt, p(1).getAsInt).asInstanceOf[AnyRef]
-  def hello(p: Array[JsonElement]): AnyRef = Hello.hello(p(0).getAsString).asInstanceOf[AnyRef]
-  def array_sum(p: Array[JsonElement]): AnyRef = {
+  def sum(p: util.List[JsonElement]): AnyRef = Sum.sum(p.get(0).getAsInt, p.get(1).getAsInt).asInstanceOf[AnyRef]
+  def hello(p: util.List[JsonElement]): AnyRef = Hello.hello(p.get(0).getAsString).asInstanceOf[AnyRef]
+  def array_sum(p: util.List[JsonElement]): AnyRef = {
     var intArray = new ListBuffer[Int]()
-    p(0).getAsJsonArray.forEach(jsonElement => intArray += jsonElement.getAsInt)
+    p.get(0).getAsJsonArray.forEach(jsonElement => intArray += jsonElement.getAsInt)
     ArraySum.sum(intArray.toList).asInstanceOf[AnyRef]
   }
-  def int_range(p: Array[JsonElement]): AnyRef = {
-    IntRange.generate(p(0).getAsInt, p(1).getAsInt).asJava.asInstanceOf[AnyRef]
+  def int_range(p: util.List[JsonElement]): AnyRef = {
+    IntRange.generate(p.get(0).getAsInt, p.get(1).getAsInt).asJava.asInstanceOf[AnyRef]
   }
-  def fizz_buzz(p: Array[JsonElement]): AnyRef = FizzBuzz.fizzBuzz(p(0).getAsInt).asInstanceOf[AnyRef]
-  def checkout(p: Array[JsonElement]): AnyRef = Checkout.checkout(p(0).getAsString).asInstanceOf[AnyRef]
+  def fizz_buzz(p: util.List[JsonElement]): AnyRef = FizzBuzz.fizzBuzz(p.get(0).getAsInt).asInstanceOf[AnyRef]
+  def checkout(p: util.List[JsonElement]): AnyRef = Checkout.checkout(p.get(0).getAsString).asInstanceOf[AnyRef]
 
 }
